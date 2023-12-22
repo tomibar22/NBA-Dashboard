1079f8c3145ff3509692180907272a0e71044f93
tomibar22 <133946417+tomibar22@users.noreply.github.com>
2023-12-22 17:01:56 +0200

    Add files via upload

--- pages/Standings (Desktop).py
+++ pages/Standings (Desktop).py
@@ -7,16 +7,16 @@ import plotly.express as px
 st.set_page_config(page_title="Standings",
                    layout="wide")
 
-logos = ['https://content.sportslogos.net/logos/6/220/thumbs/22081902021.gif', 'https://content.sportslogos.net/logos/6/213/thumbs/slhg02hbef3j1ov4lsnwyol5o.gif', 'https://content.sportslogos.net/logos/6/3786/thumbs/hsuff5m3dgiv20kovde422r1f.gif', 'https://content.sportslogos.net/logos/6/5120/thumbs/512019262015.gif', 'https://content.sportslogos.net/logos/6/221/thumbs/hj3gmh82w9hffmeh3fjm5h874.gif', 'https://content.sportslogos.net/logos/6/222/thumbs/22253692023.gif',
-         'https://content.sportslogos.net/logos/6/228/thumbs/22834632018.gif', 'https://content.sportslogos.net/logos/6/229/thumbs/22989262019.gif', 'https://content.sportslogos.net/logos/6/223/thumbs/22321642018.gif', 'https://content.sportslogos.net/logos/6/235/thumbs/23531522020.gif', 'https://content.sportslogos.net/logos/6/230/thumbs/23068302020.gif', 'https://content.sportslogos.net/logos/6/224/thumbs/22448122018.gif',
-         'https://content.sportslogos.net/logos/6/236/thumbs/23637762019.gif', 'https://content.sportslogos.net/logos/6/237/thumbs/23773242024.gif', 'https://content.sportslogos.net/logos/6/231/thumbs/23143732019.gif', 'https://content.sportslogos.net/logos/6/214/thumbs/burm5gh2wvjti3xhei5h16k8e.gif', 'https://content.sportslogos.net/logos/6/225/thumbs/22582752016.gif', 'https://content.sportslogos.net/logos/6/232/thumbs/23296692018.gif', 
-         'https://content.sportslogos.net/logos/6/4962/thumbs/496292922024.gif', 'https://content.sportslogos.net/logos/6/216/thumbs/21671702024.gif', 'https://content.sportslogos.net/logos/6/2687/thumbs/khmovcnezy06c3nm05ccn0oj2.gif', 'https://content.sportslogos.net/logos/6/217/thumbs/wd9ic7qafgfb0yxs7tem7n5g4.gif', 'https://content.sportslogos.net/logos/6/218/thumbs/21870342016.gif', 'https://content.sportslogos.net/logos/6/238/thumbs/23843702014.gif',
-         'https://content.sportslogos.net/logos/6/239/thumbs/23997252018.gif', 'https://content.sportslogos.net/logos/6/240/thumbs/24040432017.gif', 'https://content.sportslogos.net/logos/6/233/thumbs/23325472018.gif', 'https://content.sportslogos.net/logos/6/227/thumbs/22770242021.gif', 'https://content.sportslogos.net/logos/6/234/thumbs/23485132023.gif', 'https://content.sportslogos.net/logos/6/219/thumbs/21956712016.gif']
+colors = ['#E03A3E', '#007A33', '#FFFFFF', '#1D1160', '#CE1141', '#860038',
+         '#00538C', '#0E2240', '#C8102E', '#1D428A', '#CE1141', '#002D62',
+         '#C8102E', '#552583', '#5D76A9', '#98002E', '#00471B', '#0C2340', 
+         '#0C2340', '#006BB6', '#007AC1', '#0077C0', '#006BB6', '#1D1160',
+         '#E03A3E', '#5A2D81', '#C4CED4', '#CE1141', '#002B5C', '#002B5C']
 
 
 df = LeagueStandings().get_data_frames()[0]
 dff = df.sort_values('TeamCity')
-dff['logo'] = logos
+dff['color'] = colors
 dff = dff.drop(columns=['LeagueID', 'SeasonID', 'TeamID', 'ConferenceRecord', 'DivisionRecord', 'DivisionRank', 'LeagueRank', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                       'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
 dff['Team'] = dff['TeamCity'] + ' ' + dff['TeamName']
@@ -29,12 +29,12 @@ west = dff[dff['Conference']=='West'].sort_values(['WinPCT','Team'], ascending=[
 west = west.reset_index(drop=True)
 west.index = west.index + 1
 
-east = east[['Team', 'Record', 'WinPCT', 'strCurrentStreak', 'L10', 'PointsPG', 'OppPointsPG', 'DiffPointsPG', 'logo']]
+east = east[['Team', 'Record', 'WinPCT', 'strCurrentStreak', 'L10', 'PointsPG', 'OppPointsPG', 'DiffPointsPG', 'color']]
 east['Status'] = east.index.to_series().apply(lambda x: 'Playoff Qualifiers' if x < 7 else ('Play-In Tournament' if x < 11 else 'Non-Playoff'))
 east['WinPCT_str'] = east['WinPCT'].map('{:.1%}'.format)
 east['Position'] = range(1, len(east) + 1)
 
-west = west[['Team', 'Record', 'WinPCT', 'strCurrentStreak', 'L10', 'PointsPG', 'OppPointsPG', 'DiffPointsPG', 'logo']]
+west = west[['Team', 'Record', 'WinPCT', 'strCurrentStreak', 'L10', 'PointsPG', 'OppPointsPG', 'DiffPointsPG', 'color']]
 west['Status'] = west.index.to_series().apply(lambda x: 'Playoff Qualifiers' if x < 7 else ('Play-In Tournament' if x < 11 else 'Non-Playoff'))
 west['WinPCT_str'] = west['WinPCT'].map('{:.1%}'.format)
 west['Position'] = range(1, len(east) + 1)
@@ -53,6 +53,7 @@ with st.container(border=True):
                     height=1050)
         
         fig.update_yaxes(categoryorder='array', categoryarray=east['Team'][::-1])
+        fig.update_xaxes(range=[0, east['WinPCT'].max()])
         fig.update_traces(hovertemplate='L10: %{customdata[0]}   DPPG: %{customdata[1]}',
                         insidetextfont=dict(size=20),
                         outsidetextfont=dict(size=20))
@@ -93,6 +94,7 @@ with st.container(border=True):
                     height=1050)
         
             fig.update_yaxes(categoryorder='total ascending')
+            fig.update_xaxes(range=[0, west['WinPCT'].max()])
             fig.update_traces(hovertemplate='L10: %{customdata[0]}   DPPG: %{customdata[1]}',
                             insidetextfont=dict(size=20),
                             outsidetextfont=dict(size=20))
